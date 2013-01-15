7f8f4aaf9b742bdc6491d99e2a6afb37389ee2e4
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-01-15 00:26:27 -0500

    Don't do the entire postgis install process every deploy

--- deploy/postgis_setup.sh
+++ deploy/postgis_setup.sh
@@ -4,27 +4,32 @@ cd
 mkdir sources
 cd sources
 
-wget http://download.osgeo.org/geos/geos-3.3.2.tar.bz2
-tar -xjf geos-3.3.2.tar.bz2
+if [ ! -d geos-3.3.2 ]
+then
+  wget http://download.osgeo.org/geos/geos-3.3.2.tar.bz2
+  tar -xjf geos-3.3.2.tar.bz2
+fi
 cd geos-3.3.2
-./configure
-make && make install
+make install || ( ./configure && make && make install && )
 cd ..
 
-wget http://download.osgeo.org/proj/proj-4.7.0.tar.gz
-tar -zxvf proj-4.7.0.tar.gz
+if [ ! -d proj-4.7.0 ]
+then
+  wget http://download.osgeo.org/proj/proj-4.7.0.tar.gz
+  tar -zxvf proj-4.7.0.tar.gz
+fi
 cd proj-4.7.0
-./configure
-make && make install
+make install || ( ./configure && make && make install && )
 cd ..
 
-wget  http://download.osgeo.org/gdal/gdal-1.9.0.tar.gz
-tar -zxvf gdal-1.9.0.tar.gz
+if [ ! -d gdal-1.9.0 ]
+then
+  wget  http://download.osgeo.org/gdal/gdal-1.9.0.tar.gz
+  tar -zxvf gdal-1.9.0.tar.gz
+fi
 cd gdal-1.9.0
-./configure
-make && make install
+make install || ( ./configure && make && make install && )
 cd ..
 
-cd sources
 #rm -r geos-3.3.2* proj-4.7.0* gdal-1.9.0*
 cd
